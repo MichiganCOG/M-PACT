29a58805a2ed03367f74ca29233027231739ff0f
Eric Hofesmann <erichof@umich.edu>
2017-12-06 16:55:12 -0500

    Test V1 on Rate dataset

--- models/tsn/tsn_preprocessing_TFRecords.py
+++ models/tsn/tsn_preprocessing_TFRecords.py
@@ -38,6 +38,10 @@ _R_MEAN = 123.68
 _G_MEAN = 116.78
 _B_MEAN = 103.94
 
+_R_STD = 58.39
+_G_STD = 57.12
+_B_STD = 57.37
+
 _RESIZE_SIDE_MIN = 256
 _RESIZE_SIDE_MAX = 512
 
@@ -210,6 +214,34 @@ def _mean_image_subtraction(image, means):
         channels[i] -= means[i]
     return tf.concat(axis=2, values=channels)
 
+def _mean_image_subtraction_and_std_normalization(image, means, stds):
+    """Subtracts the given means from each image channel.
+    For example:
+        means = [123.68, 116.779, 103.939]
+        stds  = [58.39, 57.12, 57.37]
+        image = mean_image_subtraction(image, means)
+    Note that the rank of `image` must be known.
+    Args:
+        image: a tensor of size [height, width, C].
+        means: a C-vector of values to subtract from each channel.
+    Returns:
+        the centered image.
+    Raises:
+        ValueError: If the rank of `image` is unknown, if `image` has a rank other
+            than three or if the number of channels in `image` doesn't match the
+            number of values in `means`.
+    """
+    if image.get_shape().ndims != 3:
+        raise ValueError('Input must be of size [height, width, C>0]')
+    num_channels = image.get_shape().as_list()[-1]
+    if len(means) != num_channels:
+        raise ValueError('len(means) must match the number of channels')
+
+    channels = tf.split(axis=2, num_or_size_splits=num_channels, value=image)
+    for i in range(num_channels):
+        channels[i] -= means[i]
+        channels[i] /= stds[i]
+    return tf.concat(axis=2, values=channels)
 
 def _smallest_size_at_least(height, width, smallest_side):
   """Computes new shape with the smallest side equal to `smallest_side`.
@@ -441,5 +473,8 @@ def preprocess(input_data_tensor, frames, height, width, channel, input_dims, ou
     input_data_tensor = tf.gather(input_data_tensor, frame_ticks)
     input_data_tensor = tf.to_float(input_data_tensor)
     output_data_tensor = preprocess_video(input_data_tensor, size, size, 256, 340, istraining)
+
+    input_data_tensor = input_data_tensor[...,::-1]
+
     labels_tensor = tf.tile( [label], [num_frames_per_video])
     return output_data_tensor, labels_tensor
