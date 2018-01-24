9b69f3aebc2e8fa2e5e1c6e4edfc29ffd597a2f7
Madan <madantrg@umich.edu>
2018-01-24 18:14:23 -0500

    Changes made without batch_norm files for resnet plus lstm

--- models/__init__.py
+++ models/__init__.py
@@ -1,20 +1,20 @@
 # Basic model imports
-from vgg16.vgg16_model                       import VGG16
+#from vgg16.vgg16_model                       import VGG16
 from resnet.resnet_model                     import ResNet
-from resnet_50_frames.resnet_50_frames_model import ResNet_50_Frames
-from c3d.c3d_model  			     import C3D
+#from resnet_50_frames.resnet_50_frames_model import ResNet_50_Frames
+#from c3d.c3d_model  			     import C3D
 
-# RAIN layer model imports
-from resnet_RIL.resnet_RIL_interp_median_model_v23_2_1    import ResNet_RIL_Interp_Median_v23_2_1
-from resnet_RIL.resnet_RIL_interp_median_model_v23_4      import ResNet_RIL_Interp_Median_v23_4
-from resnet_RIL.resnet_RIL_interp_median_model_v23_7_1    import ResNet_RIL_Interp_Median_v23_7_1
-
-from resnet_RIL.resnet_RIL_interp_median_model_v31_3      import ResNet_RIL_Interp_Median_v31_3
-from resnet_RIL.resnet_RIL_interp_median_model_v34_3_lstm import ResNet_RIL_Interp_Median_v34_3_lstm
-
-from resnet_RIL.resnet_RIL_interp_median_model_v35_lstm   import ResNet_RIL_Interp_Median_v35_lstm
-from resnet_RIL.resnet_RIL_interp_median_model_v36_lstm   import ResNet_RIL_Interp_Median_v36_lstm
-from resnet_RIL.resnet_RIL_interp_median_model_v37_lstm   import ResNet_RIL_Interp_Median_v37_lstm
-from resnet_RIL.resnet_RIL_interp_median_model_v38        import ResNet_RIL_Interp_Median_v38
-from resnet_RIL.resnet_RIL_interp_median_model_v39        import ResNet_RIL_Interp_Median_v39
-from resnet_RIL.resnet_RIL_interp_median_model_v40        import ResNet_RIL_Interp_Median_v40
+## RAIN layer model imports
+#from resnet_RIL.resnet_RIL_interp_median_model_v23_2_1    import ResNet_RIL_Interp_Median_v23_2_1
+#from resnet_RIL.resnet_RIL_interp_median_model_v23_4      import ResNet_RIL_Interp_Median_v23_4
+#from resnet_RIL.resnet_RIL_interp_median_model_v23_7_1    import ResNet_RIL_Interp_Median_v23_7_1
+#
+#from resnet_RIL.resnet_RIL_interp_median_model_v31_3      import ResNet_RIL_Interp_Median_v31_3
+#from resnet_RIL.resnet_RIL_interp_median_model_v34_3_lstm import ResNet_RIL_Interp_Median_v34_3_lstm
+#
+#from resnet_RIL.resnet_RIL_interp_median_model_v35_lstm   import ResNet_RIL_Interp_Median_v35_lstm
+#from resnet_RIL.resnet_RIL_interp_median_model_v36_lstm   import ResNet_RIL_Interp_Median_v36_lstm
+#from resnet_RIL.resnet_RIL_interp_median_model_v37_lstm   import ResNet_RIL_Interp_Median_v37_lstm
+#from resnet_RIL.resnet_RIL_interp_median_model_v38        import ResNet_RIL_Interp_Median_v38
+#from resnet_RIL.resnet_RIL_interp_median_model_v39        import ResNet_RIL_Interp_Median_v39
+#from resnet_RIL.resnet_RIL_interp_median_model_v40        import ResNet_RIL_Interp_Median_v40
